b2b33650a8466cd50fe83be873dc1eb0274f7662
Giedre <Giedre@GIEDRE-PC>
2018-02-01 19:07:47 +0200

    Uzduotys

---
+++ 16_kontroline_uzd_20170201/Miestas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_kontroline_uzd_20170201
+{
+    class Miestas
+    {
+        public string Pavadinimas;
+        public int GyventojuSkaicius;
+        public double PlotasKvKm;
+    }
+}
